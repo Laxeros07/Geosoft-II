cfb37e8619a78b0f6df42584708698ffcadc9e40
Lorano98 <evalangstein@freenet.de>
2022-11-07 15:04:09 +0100

    R-integration

--- routes/upload.js
+++ routes/upload.js
@@ -1,9 +1,15 @@
 var express = require("express");
 var router = express.Router();
+var R = require("r-integration");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("Upload", { title: "Upload" });
+  res.render("Upload", { title: "Upload", p: R });
+});
+
+router.post("/", function (req, res, next) {
+  let result = R.executeRCommand("max(1, 2, 3)");
+  console.log(result);
 });
 
 module.exports = router;
