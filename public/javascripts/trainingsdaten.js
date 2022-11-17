a651b893127b8b5dbf105e5376e6a759b10665b4
Lorano98 <evalangstein@freenet.de>
2022-11-17 11:04:03 +0100

    Upload geojson

--- public/javascripts/trainingsdaten.js
+++ public/javascripts/trainingsdaten.js
@@ -1,3 +1,12 @@
+//var trainingsdaten = null;
+var trainingsdatenInput = document.getElementById("trainingsdatenInput");
+var trainingsdatenHochladen = document.getElementById(
+  "trainingsdatenHochladen"
+);
+
+trainingsdatenInput.addEventListener("change", fileChange);
+trainingsdatenHochladen.addEventListener("click", uploadTrainingsdaten);
+
 // Karte mit Zentrum definieren
 var map = L.map("map").setView([52, 7.6], 10);
 
@@ -7,4 +16,38 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-function uploadTrainingsdaten() {}
+if (trainingsdaten != null) {
+  console.log(trainingsdaten);
+}
+
+/**
+ * Wird ausgeführt, wenn eine Datei hochgeladen wurde.
+ * Quelle: https://stackoverflow.com/questions/23344776/how-to-access-data-of-uploaded-json-file
+ * @param {*} event
+ */
+function fileChange(event) {
+  var reader = new FileReader();
+  reader.onload = (event) => {
+    trainingsdaten = event.target.result;
+    console.log(trainingsdaten);
+    //console.log(trainingsdatenInput);
+
+    //uploadTrainingsdaten();
+  };
+  reader.readAsText(event.target.files[0]);
+  /*
+  const dataTransfer = new DataTransfer();
+  dataTransfer.items.add(event.target.files[0]); //your file(s) reference(s)
+  trainingsdatenInput.files = dataTransfer.files;
+  */
+}
+
+function uploadTrainingsdaten() {
+  fetch("http://localhost:3000/upload", {
+    method: "POST", // or 'PUT'
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: trainingsdaten,
+  });
+}
--- routes/upload.js
+++ routes/upload.js
@@ -1,12 +1,44 @@
 var express = require("express");
 var router = express.Router();
 var R = require("r-integration");
+const MongoClient = require("mongodb").MongoClient;
+const app = require("../app");
+
+const url = "mongodb://localhost:27017"; // connection URL
+const client = new MongoClient(url); // mongodb client
+const dbName = "Apollo13"; // database name
+const collectionName = "trainingsdaten"; // collection nam
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("Upload", { title: "Upload", data: "", radius: "", selected: "" });
+  res.render("Upload", { title: "Upload", trainingsdaten: null, radius: "" });
 });
 
+// Wird ausgeführt, wenn der Speichern Button gedrückt wurde
+router.post("/", function (req, res, next) {
+  console.log("Ergebnis:");
+  console.log(req.body);
+
+  // connect to the mongodb database and afterwards, insert one the new element
+  client.connect(function (err) {
+    console.log("Connected successfully to server");
+
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
+
+    // Insert the document in the database
+    collection.insertMany(req.body.features, () => loadPage(req.body, res));
+  });
+});
+
+function loadPage(features, res) {
+  res.render("Upload", {
+    title: "Upload",
+    trainingsdaten: features,
+    radius: "",
+  });
+}
+/*
 router.post("/", function (req, res, next) {
   let result;
   switch (req.body.select) {
@@ -22,10 +54,9 @@ router.post("/", function (req, res, next) {
 
   res.render("Upload", {
     title: "Upload",
-    data: result,
+    trainingsdaten: null,
     radius: req.body.radius,
-    selected: req.body.select,
   });
 });
-
+*/
 module.exports = router;
