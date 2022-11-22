efd587065477acecd34ae3c2be794e8fcd9610cc
Felioxx <102677497+Felioxx@users.noreply.github.com>
2022-11-22 14:44:43 +0100

    trainingsdaten angepasst, rasterdaten hinzugefügt (die Beispieldaten)

--- public/javascripts/leaflet.js
+++ public/javascripts/leaflet.js
@@ -4,7 +4,6 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-
 let x = document.getElementById("coordinates");
 /**
  * Returns the Geolocation of the browser
