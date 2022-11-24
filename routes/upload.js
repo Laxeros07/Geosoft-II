b0a7a80308b37696f94d87eabd91adf7b827d042
Lorano98 <evalangstein@freenet.de>
2022-11-24 16:22:53 +0100

    rasterdaten hochladen

--- routes/upload.js
+++ routes/upload.js
@@ -22,47 +22,45 @@ router.get("/", function (req, res, next) {
 });
 
 // Wird ausgeführt, wenn der Speichern Button gedrückt wurde
-router.post(
-  "/",
-  /*upload.single("file"), */ function (req, res, next) {
-    /*
-    let file = req;
-    console.log(file.body);
-    //await upload(file);
-    res.send({ file: file.body });*/
+router.post("/", function (req, res, next) {
+  if (req.body.select) {
+    let result = R.callMethod("public/rScripts/flaeche.r", "x", {
+      radius: parseInt(req.body.radius),
+    });
+    res.render("upload", {
+      title: "Upload",
+      trainingsdaten: null,
+      radius: req.body.radius,
+      result: result,
+    });
+  } else if (req.body.epsg && req.body.bbox) {
+    let result = R.callMethod("public/rScripts/coordConvers.r", "x", {
+      epsg: req.body.epsg,
+      bbox: req.body.bbox,
+    });
 
-    if (req.body.select) {
-      let result = R.callMethod("public/rScripts/flaeche.r", "x", {
-        radius: parseInt(req.body.radius),
+    res.send(result);
+  } else {
+    // connect to the mongodb database and afterwards, insert one the new element
+    client.connect(function (err) {
+      console.log("Connected successfully to server");
+
+      const db = client.db(dbName);
+      const collection = db.collection(collectionName);
+
+      trainingsdaten = req.body;
+
+      // Insert the document in the database
+      collection.insertMany(trainingsdaten.features, function (err, result) {
+        console.log(
+          `Inserted ${result.insertedCount} document into the collection`
+        );
+        console.log(trainingsdaten);
+        res.send(trainingsdaten);
       });
-      res.render("upload", {
-        title: "Upload",
-        trainingsdaten: null,
-        radius: req.body.radius,
-        result: result,
-      });
-    } else {
-      // connect to the mongodb database and afterwards, insert one the new element
-      client.connect(function (err) {
-        console.log("Connected successfully to server");
-
-        const db = client.db(dbName);
-        const collection = db.collection(collectionName);
-
-        trainingsdaten = req.body;
-
-        // Insert the document in the database
-        collection.insertMany(trainingsdaten.features, function (err, result) {
-          console.log(
-            `Inserted ${result.insertedCount} document into the collection`
-          );
-          console.log(trainingsdaten);
-          res.send(trainingsdaten);
-        });
-      });
-    }
+    });
   }
-);
+});
 
 router.put("/", function (req, res, next) {
   console.log(req);
