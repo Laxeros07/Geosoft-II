dee21cdf8557474f155e217e1b60dd89ac6c26cc
pmunding <102729357+pmunding@users.noreply.github.com>
2022-12-07 14:01:46 +0100

    Trainingspolygone

--- routes/result.js
+++ routes/result.js
@@ -6,4 +6,16 @@ router.get("/", function (req, res, next) {
   res.render("result", { title: "Result" });
 });
 
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+
+router.post("/", upload.single("trainingsdaten"), uploadFiles);
+
+function uploadFiles(req, res) {
+  console.log("lol");
+  console.log("Hallo");
+  console.log("Ciao");
+  res.json({ message: "Successfully uploaded files" });
+}
+
 module.exports = router;
