a1f590a394ee4b40fa36c880e8ff808bc0367616
Lorano98 <evalangstein@freenet.de>
2022-12-08 11:10:38 +0100

    Aufräumen

--- routes/upload.js
+++ routes/upload.js
@@ -40,10 +40,8 @@ router.get("/", function (req, res, next) {
   res.render("upload", { title: "Upload", radius: "", result: "" });
 });
 
-// Wird ausgeführt, wenn der Speichern Button gedrückt wurde
-
+// Läd die Daten in den Upload Ordner hoch
 router.post("/", upload.single("daten"), uploadFiles);
-//router.post("/", upload.single("rasterdaten"), uploadFiles);
 
 function uploadFiles(req, res) {
   console.log("lol");
@@ -51,35 +49,5 @@ function uploadFiles(req, res) {
   console.log(filetype);
   res.send({ message: filetype });
 }
-
-// req.file is the `avatar` file
-// req.body will hold the text fields, if there were any
-
-router.put("/", function (req, res, next) {
-  console.log(req);
-  res.send({ data: "Fertig" });
-});
-
-router.post("/rSkript", function (req, res, next) {
-  //res.redirect("/upload");
-  let result;
-  switch (req.body.select) {
-    case "flaeche":
-      result = R.callMethod("public/rScripts/flaeche.r", "x", {
-        radius: parseInt(req.body.radius),
-      });
-      break;
-    case "max":
-      result = R.executeRScript("public/rScripts/test.r");
-      break;
-  }
-
-  res.render("upload", {
-    title: "Upload",
-    trainingsdaten: null,
-    radius: req.body.radius,
-    result: result,
-  });
-});
 
 module.exports = router;
