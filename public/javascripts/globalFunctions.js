9f8df25e04efbc2e24669c497f03481c527223e3
Felioxx <102677497+Felioxx@users.noreply.github.com>
2022-12-19 16:27:35 +0100

    beispielklassifikation und aoa hinzugefügt

--- public/javascripts/globalFunctions.js
+++ public/javascripts/globalFunctions.js
@@ -1,7 +1,102 @@
 function addGeotiffToMap(url) {
-  var url = "../beispieldaten/sentinelRaster2_umprojiziert.tif";
-
   fetch(url)
+    .then((response) => response.arrayBuffer())
+    .then((arrayBuffer) => {
+      parseGeoraster(arrayBuffer).then((georaster) => {
+        console.log("georaster:", georaster);
+
+        var layer = new GeoRasterLayer({
+          georaster: georaster,
+          resolution: 256,
+          pixelValuesToColorFn: (values) => {
+            let maxs = georaster.maxs;
+            let mins = georaster.mins;
+
+            values[0] = Math.round(
+              (255 / (4000 - mins[0])) * (values[0] - mins[0])
+            );
+            values[1] = Math.round(
+              (255 / (4000 - mins[1])) * (values[1] - mins[1])
+            );
+            values[2] = Math.round(
+              (255 / (4000 - mins[2])) * (values[2] - mins[2])
+            );
+
+            // make sure no values exceed 255
+            values[0] = Math.min(values[0], 255);
+            values[1] = Math.min(values[1], 255);
+            values[2] = Math.min(values[2], 255);
+
+            // treat all black as no data
+            if (values[0] === 0 && values[1] === 0 && values[2] === 0)
+              return null;
+
+            return `rgb(${values[2]}, ${values[1]}, ${values[0]})`;
+          },
+          /*
+        pixelValuesToColorFn: (values) =>
+          values[0] > 255
+            ? null
+            : `rgb(${values[0]},${values[1]},${values[2]})`,*/
+        });
+        layer.addTo(map);
+
+        layerControl.addOverlay(layer, "Rasterbild");
+
+        map.fitBounds(layer.getBounds());
+      });
+    });
+}
+
+function addPredictionAndAoaToMap(predUrl, aoaUrl) {
+  fetch(predUrl)
+    .then((response) => response.arrayBuffer())
+    .then(parseGeoraster)
+    .then((georaster) => {
+      console.log("georaster:", georaster);
+
+      var layer = new GeoRasterLayer({
+        georaster: georaster,
+        resolution: 256,
+        pixelValuesToColorFn: (values) => {
+          let maxs = georaster.maxs;
+          let mins = georaster.mins;
+
+          values[0] = Math.round(
+            (255 / (4000 - mins[0])) * (values[0] - mins[0])
+          );
+          values[1] = Math.round(
+            (255 / (4000 - mins[1])) * (values[1] - mins[1])
+          );
+          values[2] = Math.round(
+            (255 / (4000 - mins[2])) * (values[2] - mins[2])
+          );
+
+          // make sure no values exceed 255
+          values[0] = Math.min(values[0], 255);
+          values[1] = Math.min(values[1], 255);
+          values[2] = Math.min(values[2], 255);
+
+          // treat all black as no data
+          if (values[0] === 0 && values[1] === 0 && values[2] === 0)
+            return null;
+
+          return `rgb(${values[2]}, ${values[1]}, ${values[0]})`;
+        },
+        /*
+        pixelValuesToColorFn: (values) =>
+          values[0] > 255
+            ? null
+            : `rgb(${values[0]},${values[1]},${values[2]})`,*/
+      });
+      layer.addTo(map);
+
+      layerControl.addOverlay(layer, "Rasterbild");
+
+      map.fitBounds(layer.getBounds());
+    });
+
+  fetch(aoaUrl)
     .then((response) => response.arrayBuffer())
     .then((arrayBuffer) => {
       parseGeoraster(arrayBuffer).then((georaster) => {
