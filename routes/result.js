10dfe12e2630226288922c8d7ae8996f2d95e651
Lorano98 <evalangstein@freenet.de>
2023-01-28 16:27:13 +0100

    Download fertig

--- routes/result.js
+++ routes/result.js
@@ -99,18 +99,12 @@ router.post("/", function (req, res, next) {
   else {
     //var zip = new JSZip();
     var dateien = [];
-    console.log("hallo");
-    console.log(req.body.aoa_datei);
+    console.log("req");
+    console.log(req.body);
     if (req.body.prediction_datei) {
       dateien.push({
         path: req.body.prediction_datei,
         name: "classification.tif",
-      });
-      fs.readFile(req.body.value, function read(err, file) {
-        if (err) {
-          throw err;
-        }
-        zip.file("AoA.tif", file);
       });
     }
     if (req.body.polygone_datei) {
@@ -144,6 +138,8 @@ router.post("/", function (req, res, next) {
     // console.log("blob:");
     // console.log(blob.files);
     // //saveAs(blob, "hello.zip");
+    console.log("Dateien:");
+    console.log(dateien);
     res.zip(dateien);
   }
 });
