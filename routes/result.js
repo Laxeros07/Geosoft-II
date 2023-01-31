a613850c4af54803c415890dc068a44a0b7d3340
Lorano98 <evalangstein@freenet.de>
2023-01-31 18:38:17 +0100

    Anpassung

--- routes/result.js
+++ routes/result.js
@@ -12,51 +12,59 @@ router.get("/", function (req, res, next) {
 });
 
 router.post("/", function (req, res, next) {
-  if (req.body.bb) {
+  console.log(req.body);
+  if (req.body.algorithmus) {
     let url = "http://172.17.0.1:7001/";
     let bbSplit = "";
-    console.log("bb: " + req.body.bb);
-    if (req.body.bb != "-") {
-      //Es wurde eine Boundingbox angegeben
-      bbSplit = req.body.bb.split(",");
+    if (req.body.bb) {
+      if (req.body.bb != "-") {
+        //Es wurde eine Boundingbox angegeben
+        bbSplit = req.body.bb.split(",");
+      }
     }
 
-    //Zusammenbauen der Url mit den Parametern
-    switch (req.body.id) {
-      case "trainingsdaten":
-        url += "result?";
-        url += "datenanteil=" + req.body.reduzieren + "&";
-        if (bbSplit != "") {
-          //Boundingbox
-          url +=
-            "ymin=" +
-            bbSplit[2] +
-            "&ymax=" +
-            bbSplit[3] +
-            "&xmin=" +
-            bbSplit[0] +
-            "&xmax=" +
-            bbSplit[1] +
-            "&";
-        }
-        break;
-      case "modell":
-        url += "resultModell?";
-        url += "datenanteil=" + req.body.reduzieren + "&";
-        if (bbSplit != "") {
-          //Boundingbox
-          url +=
-            "ymin=" +
-            bbSplit[2] +
-            "ymax=" +
-            bbSplit[3] +
-            "xmin=" +
-            bbSplit[0] +
-            "xmax=" +
-            bbSplit[1] +
-            "&";
-        }
-        break;
+    if (req.body.id) {
+      //Zusammenbauen der Url mit den Parametern
+      switch (req.body.id) {
+        case "trainingsdaten":
+          url += "result?";
+          url += "datenanteil=" + req.body.reduzieren + "&";
+          if (bbSplit != "") {
+            //Boundingbox
+            url +=
+              "ymin=" +
+              bbSplit[2] +
+              "&ymax=" +
+              bbSplit[3] +
+              "&xmin=" +
+              bbSplit[0] +
+              "&xmax=" +
+              bbSplit[1] +
+              "&";
+          }
+          break;
+        case "modell":
+          url += "resultModell?";
+          url += "datenanteil=" + req.body.reduzieren + "&";
+          if (bbSplit != "") {
+            //Boundingbox
+            url +=
+              "ymin=" +
+              bbSplit[2] +
+              "ymax=" +
+              bbSplit[3] +
+              "xmin=" +
+              bbSplit[0] +
+              "xmax=" +
+              bbSplit[1] +
+              "&";
+          }
+          break;
+      }
+    } else {
+      url += "result?";
+      url += "datenanteil=" + req.body.reduzieren + "&";
+      //url += "datenanteil=10&";
     }
     if (req.body.algorithmus == "rf") {
       // Random Forest
