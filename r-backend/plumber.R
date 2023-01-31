364b72458cbd52fda35c9d7d638711fa80531e16
Felioxx <102677497+Felioxx@users.noreply.github.com>
2023-01-31 14:42:50 +0100

    Beispielrasterdaten und Datenanteil
    Beispielrasterdaten verkleinert und Datenanteil der verwendeten Trainingsdaten im Algorithmus nun einstellbar

--- r-backend/plumber.R
+++ r-backend/plumber.R
@@ -39,7 +39,7 @@ function() {
 #* @param ymin,ymax,xmin,xmax If provided, Zuschnitt fuer die Rasterdaten
 #* @get /result
 #* @serializer png
-function(ymin = NA, ymax = NA, xmin = NA, xmax = NA, baumAnzahl = NA, baumTiefe = NA, algorithmus = NA) {
+function(ymin = NA, ymax = NA, xmin = NA, xmax = NA, baumAnzahl = NA, baumTiefe = NA, algorithmus = NA, datenanteil = NA) {
   library(terra)
   library(sf)
   library(caret)
@@ -93,19 +93,18 @@ function(ymin = NA, ymax = NA, xmin = NA, xmax = NA, baumAnzahl = NA, baumTiefe 
 
   # Modell trainieren
   # nicht alle Daten verwenden um Rechenzeit zu sparen
-  # extr_subset <- extr[createDataPartition(extr$ID, p = 0.2)$Resample1, ]
+  extr_subset <- extr[createDataPartition(extr$ID, p = datenanteil)$Resample1, ]
 
   # eventuell Daten limitieren.
   # Verhälnis der Daten aus jedem Trainingsgebiet soll aber gleich bleiben
-  # hier:10% aus jedem Trainingsgebiet (see ?createDataPartition)
-  # trainIDs <- createDataPartition(extr$ID, p = 0.1, list = FALSE)
-  # trainDat <- extr[trainIDs, ]
+  trainIDs <- createDataPartition(extr$ID, p = datenanteil, list = FALSE)
+  trainDat <- extr[trainIDs, ]
   # Sicherstellen das kein NA in Prädiktoren enthalten ist:
-  # trainDat <- trainDat[complete.cases(trainDat[, predictors]), ]
-  trainDat <- extr[complete.cases(extr[, predictors]), ]
+  trainDat <- trainDat[complete.cases(trainDat[, predictors]), ]
+  # trainDat <- extr[complete.cases(extr[, predictors]), ]
 
-  print(algorithmus)
-  print(class(algorithmus))
+ # print(algorithmus)
+ # print(class(algorithmus))
   if(algorithmus == "rf") {
     # Hyperparameter für Modelltraining abfragen
     if (is.na(baumAnzahl)) {
